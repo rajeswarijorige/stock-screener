d493ed1f56424fdfa3a4eb628427e0793ad73aa8
rajeswarijorige <116812250+rajeswarijorige@users.noreply.github.com>
2022-10-28 09:29:45 +0530

    Update loadStockData.c

--- ProjectStockScreener1/Cut/code/project/src/loadStockData.c
+++ ProjectStockScreener1/Cut/code/project/src/loadStockData.c
@@ -13,7 +13,7 @@ struct NODE *node=NULL;
 /*load data into the linked list*/
 void loadStockData(FILE** fptr){
   
-    int count=0;
+    
     char parsedLine[PARSEDLEN];
     int row = 0;
      while(fgets(parsedLine,PARSEDLEN,*fptr) != NULL){
@@ -45,12 +45,12 @@ void loadStockData(FILE** fptr){
    
     struct NODE* avghead= head;
     printf("Date\t\tOpen\t\tHigh\t\tLow\t\tClose\t\tAdj Close\tVolume\n\n");
-    while(head!=NULL && count < 10 ){
+    while(head!=NULL){
         printf("%s\t%s\t%s\t%s\t%s\t%s\t%d\n",head->date,head->open,head->high,head->low,head->close,head->adjClose,atoi(head->volume));
         head=head->next;
-        count++;
+       
     }
-    count=0;
+    
 /*shows the recommendation*/
     showReco(avghead);
 
